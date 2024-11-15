db4eaef17a81d197384e12a62d292da14862286d
shirajahmed <shirajad690@gmail.com>
2024-11-15 13:17:54 +0530

    route exammle

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
+// app.use("/", (req, res) => {
+//   res.send("Shiraj");
+// });
+app.get("/user", (req, res) => {
+  res.send({ name: "nammee", age: 20 });
+});
+
+app.post("/user", (req, res) => {
+  console.log("save dta to db");
+  res.send("Data suffullfy save");
+});
+
 app.use("/test", (req, res) => {
   res.send("hello i ma from server");
 });
